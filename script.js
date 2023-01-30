830bac8ed5c8936351610e7a28632e06ab51ccb8
Paulina Trojanowska <paulinatrojanowskx@gmail.com>
2023-01-30 18:19:17 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "New Heading";
+}
+
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
+ 
